a80a8bf26e104f30b1781b667186c5030a371354
Arshdeep Arora <arsharora017@gmail.com>
2023-01-12 00:11:42 -0500

    add eventListner and change gridItem color.

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ function createGrid(sqCount) {
   for (let i = 0; i < sqCount; i++) {
     const gridItem = document.createElement("div");
     container.append(gridItem);
-    gridItem.textContent = `${i + 1}`;
+    // gridItem.textContent = `${i + 1}`;
     gridItem.classList.add("grid-item");
+    gridItem.addEventListener("mouseover", updateBack);
   }
   container.style.setProperty(
     `grid-template-columns`,
     `repeat(${grid.col},1fr)`
   );
+}
+
+function updateBack(e) {
+  const ele = e.target;
+  ele.style.backgroundColor = "red";
 }
